a2d139bd7f166225ae36dd4a6862e746f0f516db
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-11-15 19:19:07 +0000

    Worked on setting up user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'picks/create'
   get 'picks/index'
   get 'picks/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'picks#index'
+  root to: 'picks#index'
 end

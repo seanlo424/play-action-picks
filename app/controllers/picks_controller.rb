60a13f6ed8c0aee342f12405eab429730b9258de
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-11-17 19:03:33 +0000

    Started styling sign in page

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -1,13 +1,22 @@
 class PicksController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, only: [:new, :create]
 
   def create
-  	@pick = Pick.create
+  	#current_user.picks.create(pick_params)
+  	#redirect_to root_path
   end
 
   def index
+  	#@pick = Pick.all
   end
 
   def update
   end
+
+  #def pick_params
+  	#params.require(:pick).permit(:picks)
+  #end
+
+ # def pick
+  #end
 end

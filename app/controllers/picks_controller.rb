f376732298bd6e1eeb4bc0fa24673f83163430f1
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-12-08 21:59:49 +0000

    Added new method in picks controller

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -8,6 +8,15 @@ class PicksController < ApplicationController
     else
       
     end
+     @games = Game.where(week: "14", season: "2019")
+    @picks = current_user.picks.where(game_id: @games.pluck(:id))
+    @new_picks = []
+    @games.each do |game|
+      unless @picks.detect {|pick| pick.game_id == game.id} 
+        @new_picks.push(Pick.new(user: current_user, game: game))
+      end
+    end
+    @new_picks.concat(@picks)
   end
 
   def create
